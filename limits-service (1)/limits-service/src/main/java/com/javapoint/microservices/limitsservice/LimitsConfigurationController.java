752467e1d890ff98bfabe764a8787b5413395c96
abc <abc@example.com>
2020-08-30 10:57:55 +0530

    Microservice

---
+++ limits-service (1)/limits-service/src/main/java/com/javapoint/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,21 @@
+package com.javapoint.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javapoint.microservices.limitsservice.bean.LimitConfiguration;
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	@GetMapping(path="/limits")
+	
+	public LimitConfiguration retriveLimitsFromConfiguration() {
+		
+		
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+
+}
